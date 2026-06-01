9f2e6cbd20a14ec86ed234df8163f78794315a58
Yash Kathiriya <yashkathiriya370@gmail.com>
2026-06-01 09:28:20 +0530

    admi apnel al page scrolbale

--- goraadmin/app/(dashboard)/layout.tsx
+++ goraadmin/app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
   return (
     <div className="h-screen overflow-hidden bg-gray-50">
       <Sidebar />
-      <div className="lg:pl-64 h-full flex flex-col overflow-hidden">
+      <div className="lg:pl-64 h-full flex flex-col overflow-y-auto">
         {children}
       </div>
     </div>
